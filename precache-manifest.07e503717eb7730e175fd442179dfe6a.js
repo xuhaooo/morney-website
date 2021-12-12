02e6b0756a03c18e52e0bfa5ba9cd253b9c27f35
xuhao <bingoxuhao@gmail.com>
2021-12-12 22:00:06 +0800

    deploy

---
+++ precache-manifest.07e503717eb7730e175fd442179dfe6a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cce1adb85e9291564ab",
+    "url": "/morney-website/css/app.720d00a8.css"
+  },
+  {
+    "revision": "db031b887123085a44c588b0b7d4ad1c",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "1cce1adb85e9291564ab",
+    "url": "/morney-website/js/app.828e8591.js"
+  },
+  {
+    "revision": "079caa42f0b54571dd1a",
+    "url": "/morney-website/js/chunk-vendors.d27a44f9.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "26ea322b079604abaa4ec3ae6f910a61",
+    "url": "/morney-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-website/robots.txt"
+  }
+]);
